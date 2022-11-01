335e357194ac1538b4a36ca5aa109c9ea0e2f01d
Lucas Emanuel da Silva <lsengenheirocivil@gmail.com>
2022-10-31 21:55:20 -0300

    feat: create initial project

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import app from "./app";
+import { AppDataSource } from "./data-source";
+
+(async () => {
+  await AppDataSource.initialize().then(() => {
+    console.log("Database connected");
+  }).catch((error) => {
+    console.log(error);
+  });
+
+  app.listen(3000, () => {
+    console.log("Server running");
+  });
+
+})();
