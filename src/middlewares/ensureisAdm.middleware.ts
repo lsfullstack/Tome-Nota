1515adcff301144fd70890b94d7e889f6d197d0f
Lucas Emanuel da Silva <lsengenheirocivil@gmail.com>
2022-11-03 11:49:50 -0300

    fix: file standardization

--- src/middlewares/ensureisAdm.middleware.ts
+++ src/middlewares/ensureisAdm.middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from "express";
 const ensureIsAdmMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const isAdm = req.user.isAdm;
 
-  if(!isAdm){
+  if (!isAdm) {
     throw new AppError("User is not admin", 401);
   }
 
