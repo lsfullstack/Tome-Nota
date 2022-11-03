82b23204ef9bc10c538d824087092daa5276d7cc
Lucas Emanuel da Silva <lsengenheirocivil@gmail.com>
2022-11-03 16:27:25 -0300

    feat: create categories documentation

--- src/middlewares/handleError.middleware.ts
+++ src/middlewares/handleError.middleware.ts
@@ -1,14 +1,14 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { AppError } from "../errors/AppError";
 
-const handleErrorMiddleware = async (error: Error, req: Request, res: Response) => {
+const handleErrorMiddleware = async (error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
       message: error.message
     });
   }
   console.log(error);
-  
+
   return res.status(500).json({
     message: "Internal server error"
   });
