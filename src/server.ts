6a617cadabab1936f7c511a4cd8260bc4eac6dd2
mariaritabelchior <mariaritabelchior@gmail.com>
2022-11-04 19:31:20 -0300

    configs

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import app from "./app";
 import AppDataSource from "./data-source";
+import "dotenv/config";
 
 (async () => {
-  await AppDataSource.initialize().then(() => {
-    console.log("Database connected");
-  }).catch((error) => {
-    console.log(error);
-  });
+  await AppDataSource.initialize()
+    .then(() => {
+      console.log("Database connected");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-  app.listen(3000, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log("Server running");
   });
-
 })();
