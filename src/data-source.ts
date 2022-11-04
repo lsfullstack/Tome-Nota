51817ff0247c89eb76f198b2d87ecb9f8b758340
NaianeReis27 <email.annereis@gmail.com>
2022-11-04 18:22:21 -0300

    feat:create docker files

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,11 +12,11 @@ const AppDataSource = new DataSource(
     :
     {
       type: "postgres",
-      host: process.env.DB_HOST,
+      host: process.env.POSTGRES_HOST,
       port: 5432,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       synchronize: false,
       logging: true,
       entities: ["src/entities/*.ts"],
