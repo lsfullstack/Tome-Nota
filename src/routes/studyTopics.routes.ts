4abb5066d96ebd9e2352cb902313afa334ed6f2c
Lucas Emanuel da Silva <lsengenheirocivil@gmail.com>
2022-11-05 10:25:00 -0300

    feat: add routes of study-topics

--- src/routes/studyTopics.routes.ts
+++ src/routes/studyTopics.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import createStudyTopicController from "../controllers/studyTopics/createStudyTopics.controller";
+import deleteStudyTopicController from "../controllers/studyTopics/deleteStudyTopic.controller";
+import listStudyTopicsController from "../controllers/studyTopics/listStudyTopics.controller";
+import retrieveStudyTopicController from "../controllers/studyTopics/retrieveStudyTopic.controller";
+import updateStudyTopicController from "../controllers/studyTopics/updateStudyTopic.controller";
 import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware";
 
 const studyTopicsRoutes = Router();
 
 studyTopicsRoutes.post("", ensureAuthMiddleware, createStudyTopicController);
+studyTopicsRoutes.get("", ensureAuthMiddleware, listStudyTopicsController);
+studyTopicsRoutes.get("/:id", ensureAuthMiddleware, retrieveStudyTopicController);
+studyTopicsRoutes.patch("/:id", ensureAuthMiddleware, updateStudyTopicController);
+studyTopicsRoutes.delete("/:id", ensureAuthMiddleware, deleteStudyTopicController);
 
 export default studyTopicsRoutes;
