0549900e4b4c0f5f2aebcf62bc77e3123cd676ac
mariaritabelchior <mariaritabelchior@gmail.com>
2022-11-06 00:14:41 -0300

    feat:create paragraphs-routes

---
+++ src/controllers/paragraphs/listTParagraphs.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import listParagraphsService from "../../services/paragraphs/listParagraphs.service";
+
+const listParagraphsController = async (req: Request, res: Response) => {
+  const id: string = req.params.id;
+  const paragraphId = await listParagraphsService(id);
+  return res.status(200).json(paragraphId);
+};
+
+export default listParagraphsController;
