12a650e8207da3494248fc236fee7d9714680985
Lucas Emanuel da Silva <lsengenheirocivil@gmail.com>
2022-11-07 14:18:52 -0300

    fix: verify update user fields

--- src/controllers/lessons/updateLesson.controller.ts
+++ src/controllers/lessons/updateLesson.controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
+import { ILessonUpdate } from "../../interfaces/lessons.interface";
 import updateLessonService from "../../services/lessons/updateLesson.service";
 
 const updateLessonController = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const { name } = req.body;
-  const updatedLesson = await updateLessonService(id, name);
+  const lesson: ILessonUpdate = req.body;
+  console.log(req.query.name);
+  const updatedLesson = await updateLessonService(id, lesson);
   return res.status(200).json(updatedLesson);
 };
 
