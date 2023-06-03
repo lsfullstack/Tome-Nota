e8cc89d594508bba1085571619f68587afa4cfe6
Lucas <lsengenheirocivil@gmail.com>
2023-06-03 13:59:39 -0300

    feat: configuration to run locally

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,8 +12,11 @@ const AppDataSource = new DataSource(
     :
     {
       type: "postgres",
-      url: process.env.DATABASE_URL,
-      ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB,
       synchronize: false,
       logging: true,
       entities: process.env.NODE_ENV === "production" ? ["dist/src/entities/*.js"] : ["src/entities/*.ts"],
